55696b0eb7d07040e6470f81d8f4a8a4746144fd
theflyginwhale <greblavelo@gmail.com>
2022-07-06 14:19:07 +0200

    complete huge chunk

---
+++ src/utils/service.ts
@@ -0,0 +1,23 @@
+import React from 'react';
+import { failure, Result, success } from './result';
+
+const accessToken =
+	'pk.eyJ1IjoibXJmbHl3aGFsZWd1eSIsImEiOiJjazNoZGFpOGswMWJsM2xsMXp6N3ZnM25pIn0.qS7D5FBXfYUqswpNrCDkYw';
+
+// https://api.mapbox.com/geocoding/v5/mapbox.places/tennveien%20157.json?types=place%2Cpostcode%2Caddress&access_token=YOUR_MAPBOX_ACCESS_TOKEN
+
+export const testMapBox = async (
+	streetName: string,
+	countryISO?: string
+): Promise<Result<any[], string>> => {
+	const cISO = countryISO ? `country=${countryISO}&` : '';
+	// country=no&
+	const endpoint = `https://api.mapbox.com/geocoding/v5/mapbox.places/${streetName}.json?${cISO}types=place%2Cpostcode%2Caddress&access_token=${accessToken}`;
+	try {
+		const response = await fetch(endpoint);
+		const data = await response.json();
+		return success(data.features);
+	} catch (err) {
+		return failure(`${err}`);
+	}
+};
