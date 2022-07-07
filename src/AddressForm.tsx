4ebd939772cb6dc1750ec6cc7e666aa2ece8e519
theflyginwhale <greblavelo@gmail.com>
2022-07-07 12:51:11 +0200

    add some improvements

--- src/AddressForm.tsx
+++ src/AddressForm.tsx
@@ -1,273 +1,262 @@
 import {
-    VStack,
-    FormControl,
-    FormLabel,
-    Select,
-    Grid,
-    GridItem,
-    Input,
-    Text,
-} from "@chakra-ui/react";
-import React, { useState, useEffect } from "react";
-import { countryList } from "./utils/country-list";
-import { InputSelect } from "./InputSelect";
-import { testMapBox } from "./utils/service";
+	VStack,
+	FormControl,
+	FormLabel,
+	Select,
+	Grid,
+	GridItem,
+	Input,
+	Text,
+} from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { countryList } from './utils/country-list';
+import { InputSelect } from './InputSelect';
+import { testMapBox } from './utils/service';
 
 interface OptionItem {
-    center: [number, number];
-    context: [
-        {
-            id: string;
-            text: string;
-            wikidata?: string;
-            short_code?: string;
-        }
-    ];
-    geometry: {
-        coordinates: [number, number];
-        type: string;
-    };
-    id: string;
-    place_name: string;
-    place_type: string[];
-    properties: {
-        accuracy: string;
-    };
-    relevance: number;
-    type: string;
+	center: [number, number];
+	context: [
+		{
+			id: string;
+			text: string;
+			wikidata?: string;
+			short_code?: string;
+		}
+	];
+	geometry: {
+		coordinates: [number, number];
+		type: string;
+	};
+	id: string;
+	place_name: string;
+	place_type: string[];
+	properties: {
+		accuracy: string;
+	};
+	relevance: number;
+	type: string;
 }
 
 export interface RipeOption {
-    streetName: string;
-    postcode: string;
-    city: string;
-    address: string;
+	streetName: string;
+	postcode: string;
+	city: string;
+	address: string;
 }
 
 export const AddressForm = () => {
-    const [country, setCountry] = useState("");
-    const [streetName, setStreetName] = useState("");
-    const [zipCode, setZipCode] = useState("");
-    const [city, setCity] = useState("");
-    const [ripeOptions, setRipeOptions] = useState<RipeOption[]>([]);
-    const [rawOptions, setRawOptions] = useState<OptionItem[]>([]);
-    const [selectedOption, setSelectedOption] = useState<
-        OptionItem | undefined
-    >(undefined);
-    const [coordinates, setCoordinates] = useState([0, 0]);
+	const [country, setCountry] = useState('');
+	const [streetName, setStreetName] = useState('');
+	const [zipCode, setZipCode] = useState('');
+	const [city, setCity] = useState('');
 
-    const prepareCountryISO = (country: string) => {
-        const countryIndex = countryList.countries.findIndex(
-            (item) => item.toLocaleLowerCase() === country.toLocaleLowerCase()
-        );
-        const countryISO =
-            countryList.isoCodes[countryIndex].toLocaleLowerCase();
-        if (countryISO.length) {
-            return countryISO;
-        }
-    };
+	const [ripeOptions, setRipeOptions] = useState<RipeOption[]>([]);
+	const [rawOptions, setRawOptions] = useState<OptionItem[]>([]);
+	const [selectedOption, setSelectedOption] = useState<OptionItem | undefined>(
+		undefined
+	);
+	const [coordinates, setCoordinates] = useState([0, 0]);
 
-    useEffect(() => {
-        const countryISO = country.length ? prepareCountryISO(country) : "";
+	const prepareCountryISO = (country: string) => {
+		const countryIndex = countryList.countries.findIndex(
+			(item) => item.toLocaleLowerCase() === country.toLocaleLowerCase()
+		);
+		const countryISO = countryList.isoCodes[countryIndex].toLocaleLowerCase();
+		if (countryISO.length) {
+			return countryISO;
+		}
+	};
+	const extractData = (option: OptionItem) => {
+		let postcode = '';
+		let city = '';
+		let country = '';
+		let coordinates = [0, 0];
+		option.context.forEach((item) => {
+			if (item.id.includes('postcode')) {
+				postcode = item.text;
+			}
+			if (item.id.includes('place')) {
+				city = item.text;
+			}
+			if (item.id.includes('country')) {
+				country = item.text;
+			}
+		});
+		coordinates = [option.center[1], option.center[0]];
 
-        if (streetName.length) {
-            testMapBox(streetName, countryISO).then((res) => {
-                if (res.isSuccess()) {
-                    const raw = res.value;
-                    setRawOptions(raw);
-                    // const ripe = raw.map((item) => `${item.text}`);
-                    const ripe: RipeOption[] = raw.map((item) => {
-                        return {
-                            streetName: item.text,
-                            postcode: item.context[0].text,
-                            city: item.context[1].text,
-                            address: item.address,
-                        };
-                    });
-                    setRipeOptions(ripe);
-                }
-            });
-        }
-    }, [streetName]);
+		return { postcode, city, country, coordinates };
+	};
 
-    const extractData = (option: OptionItem) => {
-        let postcode = "";
-        let city = "";
-        let country = "";
-        let coordinates = [0, 0];
-        option.context.forEach((item) => {
-            if (item.id.includes("postcode")) {
-                postcode = item.text;
-            }
-            if (item.id.includes("place")) {
-                city = item.text;
-            }
-            if (item.id.includes("country")) {
-                country = item.text;
-            }
-        });
-        coordinates = [option.center[1], option.center[0]];
+	// Effect to fetch data from API
+	useEffect(() => {
+		const countryISO = country.length ? prepareCountryISO(country) : '';
 
-        return { postcode, city, country, coordinates };
-    };
+		if (streetName.length) {
+			testMapBox(streetName, countryISO).then((res) => {
+				if (res.isSuccess()) {
+					const raw = res.value;
+					setRawOptions(raw);
+					const ripe: RipeOption[] = raw.map((item) => {
+						return {
+							streetName: item.text,
+							postcode: item.context[0].text,
+							city: item.context[1].text,
+							address: item.address,
+						};
+					});
+					setRipeOptions(ripe);
+				}
+			});
+		}
+	}, [streetName]);
 
-    useEffect(() => {
-        if (selectedOption) {
-            const { postcode, city, country, coordinates } =
-                extractData(selectedOption);
-            setCity(city);
-            setZipCode(postcode);
-            setCountry(country);
-            setCoordinates(coordinates);
-        }
-    }, [selectedOption]);
+	// Effect to set data based on selection
+	useEffect(() => {
+		if (selectedOption) {
+			const { postcode, city, country, coordinates } =
+				extractData(selectedOption);
+			setCity(city);
+			setZipCode(postcode);
+			setCountry(country);
+			setCoordinates(coordinates);
+		}
+	}, [selectedOption]);
 
-    return (
-        <VStack>
-            <Grid
-                templateColumns="repeat(3, 1fr)"
-                templateRows="repeat(4, 1fr)"
-                gap={4}
-            >
-                <GridItem colSpan={3}>
-                    <CountryFormItem
-                        country={country}
-                        setCountry={setCountry}
-                    />
-                </GridItem>
-                <GridItem colSpan={3}>
-                    <StreetNameFormItem
-                        streetName={streetName}
-                        setStreetName={setStreetName}
-                        options={ripeOptions}
-                        rawOptions={rawOptions}
-                        setSelectedOption={setSelectedOption}
-                    />
-                </GridItem>
-                <GridItem colSpan={1}>
-                    <ZipCodeFormItem
-                        zipCode={zipCode}
-                        setZipCode={setZipCode}
-                    />
-                </GridItem>
-                <GridItem colSpan={2}>
-                    <CityFormItem city={city} setCity={setCity} />
-                </GridItem>
-                <GridItem>
-                    <CoordinatesItem coordinates={coordinates} />
-                </GridItem>
-            </Grid>
-        </VStack>
-    );
+	return (
+		<VStack>
+			<Grid
+				templateColumns="repeat(3, 1fr)"
+				templateRows="repeat(4, 1fr)"
+				gap={4}
+			>
+				<GridItem colSpan={3}>
+					<CountryFormItem country={country} setCountry={setCountry} />
+				</GridItem>
+				<GridItem colSpan={3}>
+					<StreetNameFormItem
+						streetName={streetName}
+						setStreetName={setStreetName}
+						options={ripeOptions}
+						rawOptions={rawOptions}
+						setSelectedOption={setSelectedOption}
+					/>
+				</GridItem>
+				<GridItem colSpan={1}>
+					<ZipCodeFormItem zipCode={zipCode} setZipCode={setZipCode} />
+				</GridItem>
+				<GridItem colSpan={2}>
+					<CityFormItem city={city} setCity={setCity} />
+				</GridItem>
+				<GridItem>
+					<CoordinatesItem coordinates={coordinates} />
+				</GridItem>
+			</Grid>
+		</VStack>
+	);
 };
 
 const CountryFormItem = ({
-    country,
-    setCountry,
+	country,
+	setCountry,
 }: {
-    country: string;
-    setCountry: (arg0: string) => void;
+	country: string;
+	setCountry: (arg0: string) => void;
 }) => {
-    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setCountry(e.target.value);
-    };
+	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setCountry(e.target.value);
+	};
 
-    return (
-        <FormControl>
-            <FormLabel htmlFor="country">Country</FormLabel>
-            <Select
-                name="country"
-                size="lg"
-                placeholder="Select country"
-                onChange={handleChange}
-                value={country}
-            >
-                {countryList.countries.map((country, index) => (
-                    <option key={index} value={country}>
-                        {country}
-                    </option>
-                ))}
-            </Select>
-        </FormControl>
-    );
+	return (
+		<FormControl>
+			<FormLabel htmlFor="country">Country</FormLabel>
+			<Select
+				name="country"
+				size="lg"
+				placeholder="Select country"
+				onChange={handleChange}
+				value={country}
+			>
+				{countryList.countries.map((country, index) => (
+					<option key={index} value={country}>
+						{country}
+					</option>
+				))}
+			</Select>
+		</FormControl>
+	);
 };
 
 const StreetNameFormItem = ({
-    streetName,
-    setStreetName,
-    options,
-    rawOptions,
-    setSelectedOption,
+	streetName,
+	setStreetName,
+	options,
+	rawOptions,
+	setSelectedOption,
 }: {
-    streetName: string;
-    setStreetName: (arg0: string) => void;
-    options: RipeOption[];
-    rawOptions: any[];
-    setSelectedOption: (arg0: any) => void;
+	streetName: string;
+	setStreetName: (arg0: string) => void;
+	options: RipeOption[];
+	rawOptions: any[];
+	setSelectedOption: (arg0: any) => void;
 }) => {
-    return (
-        <FormControl>
-            <FormLabel htmlFor="streetName">Street name</FormLabel>
-            <InputSelect
-                size="lg"
-                options={options}
-                rawOptions={rawOptions}
-                input={streetName}
-                setInput={setStreetName}
-                setSelectedOption={setSelectedOption}
-            />
-        </FormControl>
-    );
+	return (
+		<FormControl>
+			<FormLabel htmlFor="streetName">Street name</FormLabel>
+			<InputSelect
+				size="lg"
+				options={options}
+				rawOptions={rawOptions}
+				input={streetName}
+				setInput={setStreetName}
+				setSelectedOption={setSelectedOption}
+			/>
+		</FormControl>
+	);
 };
 
 const ZipCodeFormItem = ({
-    zipCode,
-    setZipCode,
+	zipCode,
+	setZipCode,
 }: {
-    zipCode: string;
-    setZipCode: (arg0: string) => void;
+	zipCode: string;
+	setZipCode: (arg0: string) => void;
 }) => {
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setZipCode(e.target.value);
-    };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setZipCode(e.target.value);
+	};
 
-    return (
-        <FormControl>
-            <FormLabel htmlFor="zipCode">Zip code</FormLabel>
-            <Input
-                size="lg"
-                name="zipCode"
-                value={zipCode}
-                onChange={handleChange}
-            />
-        </FormControl>
-    );
+	return (
+		<FormControl>
+			<FormLabel htmlFor="zipCode">Zip code</FormLabel>
+			<Input size="lg" name="zipCode" value={zipCode} onChange={handleChange} />
+		</FormControl>
+	);
 };
 
 const CityFormItem = ({
-    city,
-    setCity,
+	city,
+	setCity,
 }: {
-    city: string;
-    setCity: (arg0: string) => void;
+	city: string;
+	setCity: (arg0: string) => void;
 }) => {
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCity(e.target.value);
-    };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setCity(e.target.value);
+	};
 
-    return (
-        <FormControl>
-            <FormLabel htmlFor="city">City</FormLabel>
-            <Input size="lg" name="city" value={city} onChange={handleChange} />
-        </FormControl>
-    );
+	return (
+		<FormControl>
+			<FormLabel htmlFor="city">City</FormLabel>
+			<Input size="lg" name="city" value={city} onChange={handleChange} />
+		</FormControl>
+	);
 };
 
 const CoordinatesItem = ({ coordinates }: { coordinates: number[] }) => {
-    return (
-        <VStack>
-            <Text fontWeight="bold">Coordinates</Text>
-            <Text>{`${coordinates[0]},${coordinates[1]}`}</Text>
-        </VStack>
-    );
+	return (
+		<VStack>
+			<Text fontWeight="bold">Coordinates</Text>
+			<Text>{`${coordinates[0]},${coordinates[1]}`}</Text>
+		</VStack>
+	);
 };
--- src/InputSelect.tsx
+++ src/InputSelect.tsx
@@ -1,151 +1,150 @@
-import React, { useState } from "react";
-import { Input, ListItem, UnorderedList, Text, VStack } from "@chakra-ui/react";
-import { RipeOption } from "./AddressForm";
+import React, { useState } from 'react';
+import { Input, ListItem, UnorderedList, Text, VStack } from '@chakra-ui/react';
+import { RipeOption } from './AddressForm';
 
 interface InputSelectProps {
-    placeholder?: string;
-    size: string;
-    input: string;
-    setInput: (arg0: string) => void;
-    options: RipeOption[];
-    rawOptions: any[];
-    setSelectedOption: (arg0: any) => void;
+	placeholder?: string;
+	size: string;
+	input: string;
+	setInput: (arg0: string) => void;
+	options: RipeOption[];
+	rawOptions: any[];
+	setSelectedOption: (arg0: any) => void;
 }
 
 export const InputSelect = ({
-    placeholder = "",
-    options,
-    size = "md",
-    input,
-    setInput,
-    rawOptions,
-    setSelectedOption,
+	placeholder = '',
+	options,
+	size = 'md',
+	input,
+	setInput,
+	rawOptions,
+	setSelectedOption,
 }: InputSelectProps) => {
-    const [activeOptionIndex, setActiveOptionIndex] = useState<number>(0);
-    const [showOptions, setShowOptions] = useState<boolean>(false);
+	const [activeOptionIndex, setActiveOptionIndex] = useState<number>(0);
+	const [showOptions, setShowOptions] = useState<boolean>(false);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.value);
-        setActiveOptionIndex(0);
-        setShowOptions(true);
-    };
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInput(e.target.value);
+		setActiveOptionIndex(0);
+		setShowOptions(true);
+	};
 
-    const determineInput = (
-        streetName: string,
-        streetAddress: string | undefined
-    ) => {
-        if (streetAddress === undefined) {
-            return streetName;
-        }
-        return `${streetName} ${streetAddress}`;
-    };
+	const determineInput = (
+		streetName: string,
+		streetAddress: string | undefined
+	) => {
+		if (streetAddress === undefined) {
+			return streetName;
+		}
+		return `${streetName} ${streetAddress}`;
+	};
 
-    const handleItemClick = (index: number) => {
-        const { streetName, address } = options[index];
-        setInput(determineInput(streetName, address));
-        setSelectedOption(rawOptions[index]);
-    };
+	const handleItemClick = (index: number) => {
+		const { streetName, address } = options[index];
+		setInput(determineInput(streetName, address));
+		setSelectedOption(rawOptions[index]);
+	};
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        //User pressed the enter key
-        if (e.keyCode === 13) {
-            setSelectedOption(rawOptions[activeOptionIndex]);
-            const { streetName, address } = options[activeOptionIndex];
-            setInput(determineInput(streetName, address));
-            setActiveOptionIndex(0);
-            setShowOptions(false);
-        }
-        // User pressed the up arrow
-        else if (e.keyCode === 38) {
-            if (activeOptionIndex === 0) {
-                return;
-            }
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		//User pressed the enter key
+		if (e.keyCode === 13) {
+			setSelectedOption(rawOptions[activeOptionIndex]);
+			const { streetName, address } = options[activeOptionIndex];
+			setInput(determineInput(streetName, address));
+			setActiveOptionIndex(0);
+			setShowOptions(false);
+		}
+		// User pressed the up arrow
+		else if (e.keyCode === 38) {
+			if (activeOptionIndex === 0) {
+				return;
+			}
 
-            setActiveOptionIndex(activeOptionIndex - 1);
-        }
-        // User pressed the down arrow
-        else if (e.keyCode === 40) {
-            if (activeOptionIndex + 1 === options.length) {
-                return;
-            }
+			setActiveOptionIndex(activeOptionIndex - 1);
+		}
+		// User pressed the down arrow
+		else if (e.keyCode === 40) {
+			if (activeOptionIndex + 1 === options.length) {
+				return;
+			}
 
-            setActiveOptionIndex(activeOptionIndex + 1);
-        }
-    };
+			setActiveOptionIndex(activeOptionIndex + 1);
+		}
+	};
 
-    const OptionsList = () => {
-        if (options.length) {
-            return (
-                <UnorderedList
-                    id="myList"
-                    pos="absolute"
-                    m={0}
-                    mt={2}
-                    bg="gray.50"
-                    borderRadius={12}
-                    zIndex={2}
-                    w="full"
-                    styleType="none"
-                    style={{ overflow: "hidden" }}
-                >
-                    {options.map((option, index) => {
-                        let bgColor;
+	const OptionsList = () => {
+		if (options.length) {
+			return (
+				<UnorderedList
+					pos="absolute"
+					m={0}
+					mt={2}
+					bg="gray.50"
+					borderRadius={12}
+					zIndex={2}
+					w="full"
+					styleType="none"
+					style={{ overflow: 'hidden' }}
+				>
+					{options.map((option, index) => {
+						let bgColor;
 
-                        //Flag the active option with a class
-                        if (index === activeOptionIndex) {
-                            bgColor = "blue.200";
-                        }
+						//Flag the active option with a class
+						if (index === activeOptionIndex) {
+							bgColor = 'blue.200';
+						}
 
-                        return (
-                            <ListItem
-                                bgColor={bgColor}
-                                key={index}
-                                onMouseEnter={() => setActiveOptionIndex(index)}
-                                style={{ cursor: "pointer" }}
-                                className="optionItem"
-                                onMouseDown={() => {
-                                    handleItemClick(index);
-                                }}
-                            >
-                                <VStack
-                                    alignItems="flex-start"
-                                    id="VStack"
-                                    px={4}
-                                    py={2}
-                                    gap={0}
-                                >
-                                    <Text fontSize="lg" fontWeight="medium">
-                                        {`${option.streetName} ${
-                                            option.address ? option.address : ""
-                                        }`}
-                                    </Text>
-                                    <Text
-                                        fontSize="md"
-                                        fontWeight="normal"
-                                        style={{ marginTop: "0" }}
-                                    >{`${option.postcode} ${option.city}`}</Text>
-                                </VStack>
-                            </ListItem>
-                        );
-                    })}
-                </UnorderedList>
-            );
-        }
-        return <></>;
-    };
+						return (
+							<ListItem
+								bgColor={bgColor}
+								key={index}
+								onMouseEnter={() => setActiveOptionIndex(index)}
+								style={{ cursor: 'pointer' }}
+								className="optionItem"
+								onMouseDown={() => {
+									handleItemClick(index);
+								}}
+							>
+								<VStack
+									alignItems="flex-start"
+									id="VStack"
+									px={4}
+									py={2}
+									gap={0}
+								>
+									<Text fontSize="lg" fontWeight="medium">
+										{`${option.streetName} ${
+											option.address ? option.address : ''
+										}`}
+									</Text>
+									<Text
+										fontSize="md"
+										fontWeight="normal"
+										style={{ marginTop: '0' }}
+									>{`${option.postcode} ${option.city}`}</Text>
+								</VStack>
+							</ListItem>
+						);
+					})}
+				</UnorderedList>
+			);
+		}
+		return <></>;
+	};
 
-    return (
-        <>
-            <Input
-                size={size}
-                placeholder={placeholder}
-                onChange={handleChange}
-                onFocus={() => setShowOptions(true)}
-                onBlur={() => setShowOptions(false)}
-                onKeyDown={handleKeyDown}
-                value={input}
-            />
-            {showOptions && input && <OptionsList />}
-        </>
-    );
+	return (
+		<>
+			<Input
+				size={size}
+				placeholder={placeholder}
+				onChange={handleChange}
+				onFocus={() => setShowOptions(true)}
+				onBlur={() => setShowOptions(false)}
+				onKeyDown={handleKeyDown}
+				value={input}
+			/>
+			{showOptions && input && <OptionsList />}
+		</>
+	);
 };
